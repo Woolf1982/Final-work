5da60d3d04cdda08b79d8a1f96fb53e235a73137
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-12-09 10:20:05 +0300

    Zadacha

---
+++ Zadacha/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача: 
+Написать программу, которая из имеющегося массива строк формирует 
+новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+выполнения алгоритма. При решении не рекомендуется пользоваться 
+коллекциями, лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите " + i + " элемент массива: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+}
+void NewArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+            Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+
+Console.WriteLine("Введите первоначальный размер элементов массива");
+int massif = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[massif];
+InputArray(array);
+Console.WriteLine();
+Console.WriteLine($"Введённый первоначальный массив: [{string.Join(", ", array)}]");
+Console.Write("Полученный массив: ");
+NewArray(array);
